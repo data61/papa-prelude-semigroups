57ea458742914775d008df5859ee534ead3c7c56
Tony Morris <tmorris@tmorris.net>
2016-08-31 11:31:40 +1000

    foldl, foldr

--- src/Papa/Prelude/Semigroups/Data/List.hs
+++ src/Papa/Prelude/Semigroups/Data/List.hs
@@ -5,10 +5,13 @@ module Papa.Prelude.Semigroups.Data.List(
 , minimum
 , (++)
 , mappend
+, foldr1
+, foldl1
+, foldl1'
 ) where
   
 import Data.List.NonEmpty(NonEmpty((:|)))
-import Data.Foldable(foldr)
+import Data.Foldable(foldr, foldl, foldl')
 import Data.Ord(Ord, max, min)
 import Data.Semigroup(Semigroup((<>)))
 
@@ -42,3 +45,23 @@ mappend ::
 mappend =
   (<>)
 
+foldr1 ::
+  (a -> a -> a)
+  -> NonEmpty a
+  -> a
+foldr1 f (h :| t) =
+  foldr f h t
+
+foldl1 ::
+  (a -> a -> a)
+  -> NonEmpty a
+  -> a
+foldl1 f (h :| t) =
+  foldl f h t
+
+foldl1' ::
+  (a -> a -> a)
+  -> NonEmpty a
+  -> a
+foldl1' f (h :| t) =
+  foldl' f h t
